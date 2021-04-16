5a50634b40cbcb056b8a8be8f0f521f9fe3c172b
Vadim Homchik <homchik@gmail.com>
2021-04-16 13:48:59 +0300

    Bump Docusaurus

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,7 @@ export default function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
   return (
-    <Layout title="Gasper" description={siteConfig.title}>
+    <Layout description={siteConfig.title}>
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
